9cf46bd5f9acd75ebc97611196baf4447882c57b
nodari-dev <nodari.pylypyshak@gmail.com>
2021-01-26 16:44:46 +0200

    Starter front-end pack

---
+++ front-end/assets/js/index.js
@@ -0,0 +1,57 @@
+
+
+//NAVIGATION ANIMATION
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 150) {
+        $(".navigation").addClass("scroll");
+    } else {
+        $(".navigation").removeClass("scroll");
+    }
+});
+
+//OPEN FILTERS ON CLICK
+$("#show-filters").on('click', function() {
+    $("#show-filters").toggleClass("active");
+    if($('.filter-container').hasClass("active")){
+        $('.filter-container').fadeOut().removeClass("active");
+    }
+    else{
+        $('.filter-container').css('display', 'flex').hide().fadeIn().addClass("active");
+    }
+});
+
+//GRAPH SHOW POP-UP CLICK
+$(".open-graph").on('click', function(event) {
+    $("body").addClass('no-scroll');
+    $(".dark-bg").fadeIn();
+    $(".graph-popup").addClass("active");
+
+});
+$("#close-graph").on('click', function(event) {
+    $("body").removeClass('no-scroll');
+    $(".dark-bg").fadeOut();
+    $(".graph-popup").removeClass("active");
+
+});
+$(document).ready(function () {
+    //RRODUCT DETAIL SHOW POP-UP CLICK
+    $(".open-product-popup").click(OpenPopup);
+
+    function OpenPopup(event) {
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+        var ParentProductEl = $(this).closest(".product-el");
+        ParentProductEl.addClass("active");
+        $("body").addClass('no-scroll');
+        $(".dark-bg").fadeIn();
+    }
+
+    $(".close-product").on('click', function(e) {
+        $("body").removeClass('no-scroll');
+        $(".dark-bg").fadeOut();
+        $(".product-el").removeClass("active");
+    });
+});
+
